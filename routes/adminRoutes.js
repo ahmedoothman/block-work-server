4f7913f0ad9869924857a87de85fcbcccc21c3d4
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2024-10-04 15:34:16 +0300

    finished update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const adminController = require('../controllers/adminController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
-
+const contractController = require('../controllers/contractController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -13,7 +13,7 @@ router.use(authController.protect); //all the comming is protected
 
 router.use(authController.restrictTo('admin'));
 router.route('/users').get(userController.getAllUsers);
-
+router.get('/contracts', contractController.getAllContracts);
 router
     .route('/users/:id')
     .get(userController.getUser)
