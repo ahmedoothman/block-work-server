18c1e591ee3976c412f75350d0e8b7c52705e8f3
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2024-10-08 23:31:46 +0300

    added stats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,5 +1,9 @@
 const catchAsync = require('../utils/catchAsync');
-
+const Job = require('../model/jobPostModel');
+const User = require('../model/userModel');
+const Contract = require('../model/contractModel');
+const Proposals = require('../model/proposalModel');
+const Wallet = require('../model/walletModel');
 /* 
 {
     "categories": [
@@ -67,4 +71,81 @@ const catchAsync = require('../utils/catchAsync');
 
 
 */
-exports.getAllStats = catchAsync(async (req, res, next) => {});
+exports.getAllStats = catchAsync(async (req, res, next) => {
+    const categories = await Job.aggregate([
+        {
+            $group: {
+                _id: '$category',
+                totalJobs: { $sum: 1 },
+            },
+        },
+        {
+            $project: {
+                categoryName: '$_id',
+                totalJobs: 1,
+                _id: 0,
+            },
+        },
+    ]);
+
+    const status = await Job.aggregate([
+        {
+            $group: {
+                _id: '$status',
+                numberOfJobs: { $sum: 1 },
+            },
+        },
+        {
+            $project: {
+                statusName: '$_id',
+                numberOfJobs: 1,
+                _id: 0,
+            },
+        },
+    ]);
+
+    const overview = {
+        Users: await User.countDocuments(),
+        Proposals: await Proposals.countDocuments(),
+        Contracts: await Contract.countDocuments(),
+        Jobs: await Job.countDocuments(),
+    };
+
+    const contracts = await Contract.aggregate([
+        {
+            $group: {
+                _id: '$status',
+                numberOfContracts: { $sum: 1 },
+            },
+        },
+        {
+            $project: {
+                status: '$_id',
+                numberOfContracts: 1,
+                _id: 0,
+            },
+        },
+    ]);
+
+    const userCounts = {
+        freelancers: {
+            count: await User.countDocuments({ role: 'freelancer' }),
+        },
+        clients: { count: await User.countDocuments({ role: 'client' }) },
+    };
+
+    const systemWalletId = process.env.SYSTEM_WALLET_ID;
+
+    const systemWallet = await Wallet.findOne({ user: systemWalletId });
+
+    const profit = systemWallet.totalBalance;
+
+    res.status(200).json({
+        categories,
+        status,
+        overview,
+        contracts,
+        userCounts,
+        profit,
+    });
+});
